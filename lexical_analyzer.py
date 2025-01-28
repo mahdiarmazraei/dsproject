a82f288a672fe06d718ea1a4c59dbc5cf56088cd
mahdiar <mahdiarmazraei@gmail.com>
2025-01-28 15:21:35 +0330

    feat: craete class DFATokenizer

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,7 +1,6 @@
 import re
 from collections import deque, defaultdict
 
-# ------------------ بخش ۱: پیاده سازی Lexical Analyzer با DFA ------------------
 class DFATokenizer:
     def __init__(self):
         self.state = 'start'
